cd82defa98295bb23bb8c81f8c38c5f536178f6f
Nick Thoresby <nick@grievousangel.co.uk>
2024-02-02 10:25:44 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+using System.Xml.Xsl;
+
+const string rootFolder = @"C:\Users\Nick\Google Drive\GAS\Financial\2023-2024\Invoices";
+const string no = "523";
+
+var xslt = new XslCompiledTransform();
+xslt.Load( Path.Combine(rootFolder, "InvoiceMonthlyBootstrap.xslt"));
+xslt.Transform(Path.Combine(rootFolder, $"{no}.xml"), Path.Combine(rootFolder, $"{no}.html"));
