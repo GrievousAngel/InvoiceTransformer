b6c287b66c053f6d7365755864127e37dff9b0e9
Nick Thoresby <nick@grievousangel.co.uk>
2024-02-02 10:37:46 +0000

    Generate html file locally and launch browser

--- Program.cs
+++ Program.cs
@@ -1,9 +1,18 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Xml.Xsl;
+using System.Diagnostics;
 
 const string rootFolder = @"C:\Users\Nick\Google Drive\GAS\Financial\2023-2024\Invoices";
 const string no = "523";
+const string outputFile = $"{no}.html";
 
 var xslt = new XslCompiledTransform();
 xslt.Load( Path.Combine(rootFolder, "InvoiceMonthlyBootstrap.xslt"));
-xslt.Transform(Path.Combine(rootFolder, $"{no}.xml"), Path.Combine(rootFolder, $"{no}.html"));
+xslt.Transform(Path.Combine(rootFolder, $"{no}.xml"), outputFile);
+
+var url = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, outputFile);
+
+Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+
+
