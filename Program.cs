2265997ab64bdd866e60f321a96d9691530c435f
Nick Thoresby <nick@grievousangel.co.uk>
2024-03-04 12:08:06 +0000

    Save to location alongside css and specifically launch chrome

--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Xml.Xsl;
 using System.Diagnostics;
 
-const string rootFolder = @"C:\Users\Nick\Google Drive\GAS\Financial\2023-2024\Invoices";
-const string no = "523";
-const string outputFile = $"{no}.html";
+const string rootFolder = @"G:\My Drive\GAS\Financial\2023-2024\Invoices";
+const string no = "524";
+var outputFile = Path.Combine(rootFolder, $"{no}.html");
 
 var xslt = new XslCompiledTransform();
-xslt.Load( Path.Combine(rootFolder, "InvoiceMonthlyBootstrap.xslt"));
+
+xslt.Load(Path.Combine(rootFolder, "InvoiceMonthlyBootstrap.xslt"));
 xslt.Transform(Path.Combine(rootFolder, $"{no}.xml"), outputFile);
 
-var url = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, outputFile);
-
-Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
-
-
+Process.Start(
+    new ProcessStartInfo(
+        @"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe",
+        $"\"{outputFile}\""
+    )
+);
